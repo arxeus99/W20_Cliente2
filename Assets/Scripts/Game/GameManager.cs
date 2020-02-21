2f55d6dbafc4647645857e9565def876bb7e46e0
arxeus99 <arxeus99@gmail.com>
2020-02-21 18:53:30 +0100

    first commit

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI playerNameText;
+    public GameObject titleScreen;
+    public GameObject gameOverScreen;
+    public bool isGameActive;
+
+    public Player player;
+
+    public GameObject leftColumn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        int playerAge = System.DateTime.Today.Year - player.BirthDay.Year;
+        if (player.BirthDay.Date > System.DateTime.Today.AddYears(-playerAge)) playerAge--;
+        playerNameText.text = player.Name+" "+playerAge;
+        scoreText.text = score.ToString();
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int randomIndex = UnityEngine.Random.Range(0, 4);
+            Instantiate(targets[randomIndex]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
